2f72ecadb2066b9ba6fc4eef0c22a73790bc5e74
Luka Valencic <lvalenci@caltech.edu>
2020-05-13 16:02:42 -0500

    updated readme and created heuristic.py

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 15_puzzle_learned_heuristic
+
+# 15-puzzle representation
+Puzzle is represented in a 4x4 np.array, 0 represents the blank tile, 1-15 represent the corresponding tiles
+
+# File Contains:
+-- sovler.py: contains code for solving puzzle and generating random 15-puzzle instance
+-- heuristic.py: contains code for evaluating a puzzle instance based on the Manhattan and Hamming metrics
---
+++ heuristic.py
@@ -0,0 +1,20 @@
+"""
+heuristic.py
+Worked on by: Luka Valencic
+"""
+import numpy as np
+
+def hamming(board):
+    """
+    computes the hamming metric on the board and returns it
+    https://en.wikipedia.org/wiki/Hamming_distance
+    """
+    TODO
+
+
+def manhattan(board):
+    """
+    computes the manhattan metric on the board and returns it
+    https://en.wikipedia.org/wiki/Taxicab_geometry
+    """
+    TODO
