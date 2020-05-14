3e6d19a8ab11b44e29e8fb69976241ef38b177bc
Luka Valencic <lvalenci@caltech.edu>
2020-05-14 00:33:33 -0500

    A* search completed

--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
 # size of the board, n in n x n
 SIZE = 4
+
+# number of moves when making random board
+NUM_MOVES = 80
--- solver.py
+++ solver.py
@@ -4,6 +4,9 @@ worked on by: Luka Valencic
 """
 import numpy as np
 import heapq as h
+import time as t
+import copy
+
 from constants import *
 
 def find_blank(board):
@@ -20,7 +23,7 @@ def is_solved(board):
     """
     given a board, returns true if the board is solved, false otherwise
     """
-    return all(board == (np.arange(SIZE ** 2) + 1))
+    return np.array_equal(board, (np.arange(16) + 1).reshape((4,4)))
 
 def inversion_count(board):
     """
@@ -36,6 +39,7 @@ def inversion_count(board):
 
     return num_inv
 
+#Deprecated, changed gen_board to not require this
 def board_solvable(board):
     """
     returns true if a board is solvable, false otherwise
@@ -47,35 +51,15 @@ def board_solvable(board):
         # if entry in even row counting from top, solvable if number of
         # inversions is odd
         if (i % 2 == 0):
-            return (num_inv % 2 == 0)
+            return (num_inv % 2 == 1)
         # if entry is in odd row counting from top, solvable if number of
         # inversions is odd
         else:
-            return (num_inv % 2 == 1)
+            return (num_inv % 2 == 0)
 
     else:
         return (num_inv % 2 == 0)
   
-def rand_board():
-    """
-    generates a random board, does not check for solvability
-    """  
-    board = np.random.permutation(SIZE ** 2) + 1
-    board = board.reshape((SIZE, SIZE))
-    return board
-
-def gen_board():
-    """
-    generates a board in a random configuration and returns it.
-    Ensures that board is solvable using board_solvable
-    """
-    made_solvable = False
-    board = None
-    while(not made_solvable):
-        board = rand_board()
-        made_solvable = board_solvable(board)
-    return board
-
 def make_move(board, old_cord, new_cord):
     """
     given a board and two indices on the board, swaps the values at the two 
@@ -108,3 +92,90 @@ def valid_moves(blank_pos):
         moves.append((i, j+1))
 
     return moves
+
+def gen_board():
+    """
+    generates a board in a random configuration and returns it.
+    Ensures that board is solvable using board_solvable
+    """
+    board = (np.arange(SIZE**2)+1).reshape((SIZE, SIZE))
+    for _ in range(NUM_MOVES):
+        blank_pos = find_blank(board)
+        moves = valid_moves(blank_pos)
+        choice = np.random.choice(len(moves))
+        board = make_move(board, blank_pos, moves[choice])
+
+    return board
+
+def solve(board, metric):
+    """given a (solvable) board and a metric, this function solves the N-puzzle
+    through A* search
+    metric must only take in argument of board as a SIZE x SIZE np array
+    board must be a SIZE x SIZE np array returned from gen_board()
+    Return (n_states, tot_time, sol_path)
+    n_states: the number of states visited during A* search,
+        this includes the start and end states
+    tot_time: the time elapsed during computation in seconds
+    sol_path: a list of all the states in the shortest path to a solved 
+        state, this includeds the start and end states"""
+    
+    # initialize all relevant variables
+    # counter so elements in heap are always unique
+    count = 0
+    start_time = t.process_time()
+    sol_path = []
+    n_states = 0
+    # used to store distances travelled to any solution so that do not 
+    # re-enqueue if distance is worse
+    visited = dict()
+    queue = [(0,0,-1,[board])]
+    """ element in heap as follows: 
+    (estimated distance to solution, 
+    current distance traveled,
+    list of boards visited in this solution path - current one is at end)"""
+    h.heapify(queue)
+
+    # run actual computation
+    solved = False
+    while(not solved):
+        (_, dist, _, path) = h.heappop(queue)
+        curr_board = path[-1]
+
+        board_rep = np.array_str(curr_board)
+        # if board already visited with a lower distance, just skip it
+        # if not already visited, then we are visiting a new state
+        if board_rep in visited:
+            if visited[board_rep] <= dist:
+                continue
+        else:
+            visited[board_rep] = dist
+            n_states += 1
+
+        if is_solved(curr_board):
+            path.reverse()
+            sol_path = path
+            solved = True
+            break
+
+
+        # generate all possible moves, execute each move, and add to queue with
+        # distance metric evaluated
+
+        blank_loc = find_blank(curr_board)
+        poss_moves = valid_moves(blank_loc)
+
+        for move in poss_moves:
+            new_board = make_move(curr_board, blank_loc, move)
+            est_dist = metric(new_board) + dist
+            new_path = copy.deepcopy(path)
+            new_path.append(new_board)
+            h.heappush(queue, (est_dist, dist + 1, count, new_path))
+            count += 1
+
+
+
+
+    # compute compute ending time and return
+    end_time = t.process_time()
+    tot_time = end_time - start_time
+    return (n_states, tot_time, sol_path)
